9c0c8a1c3d3fb3c08f6bef78e5f78aebf938fc25
truongchinh1211 <nntchinh2001@gmail.com>
2022-11-06 15:13:26 +0700

    Insert statistic_GUI + Bú

---
+++ src/DTO/statisticalObject.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package DTO;
+
+/**
+ *
+ * @author Bum
+ */
+public class statisticalObject {
+
+    private String name;
+    private String size;
+    private int value;
+
+    public statisticalObject() {
+    }
+
+    public statisticalObject(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+    
+}
