3b40c4ef9f15015d3e75163ad171a1f8dd835c39
baseboy1102002 <trannguyenvietthai@gmail.com>
2022-11-10 14:11:39 +0700

    chinh sua connectDB

--- src/DAO/connectDB.java
+++ src/DAO/connectDB.java
@@ -3,26 +3,42 @@ package DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class connectDB {
-    protected Connection conn=null;
+
     public connectDB() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            try {
-                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/fast_food_store", "root", "");
-//                System.out.println("Database connection successfull!");
-            } catch (SQLException ex) {
-                Logger.getLogger(connectDB.class.getName()).log(Level.SEVERE, null, ex);
-            }
+//            try {
+//                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/fast_food_store", "root", "");
+////                System.out.println("Database connection successfull!");
+//            } catch (SQLException ex) {
+//                Logger.getLogger(connectDB.class.getName()).log(Level.SEVERE, null, ex);
+//            }
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(connectDB.class.getName()).log(Level.SEVERE, null, ex);
         }
         
     }
+    
+    
+    public Connection getConnect() {
+        try {    
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/fast_food_store", "root", "");
+            return conn;
+        } catch (SQLException ex) {
+            Logger.getLogger(connectDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    
     public static void main(String[] args) {
         connectDB DA = new connectDB();
     }
