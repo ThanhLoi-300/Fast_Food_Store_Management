a167a01dd825bbcf7252d1eca04d5b0c59340f10
giaphat-nham <phatnham3010@gmail.com>
2022-11-15 09:21:52 +0700

    Sua loi update, parse so

--- src/GUI/Product_GUI.java
+++ src/GUI/Product_GUI.java
@@ -670,7 +670,8 @@ public class Product_GUI extends javax.swing.JPanel {
         } else {
             int i = tblProductList1.getSelectedRow();
             Product_DTO selectedProduct = listProduct.get(i);
-            Product_DTO product = new Product_DTO(selectedProduct.getProductID(), txtSizeID1.getText(), txtProductName1.getText(), cbbCategoryID1.getSelectedItem() + "", Integer.parseInt(txtPrice1.getText()), Integer.parseInt(txtQuantity1.getText()), chosenImg, false, selectedBusinessStatus());
+            Product_DTO product = new Product_DTO(selectedProduct.getProductID(), txtSizeID1.getText(), txtProductName1.getText(), cbbCategoryID1.getSelectedItem() + "", Double.parseDouble(txtPrice1.getText()), Integer.parseInt(txtQuantity1.getText()), chosenImg, false, selectedBusinessStatus());
+            chosenImg = chosenImg.replace("\\", "/");
             if (productBUS.updateProduct(product,selectedProduct.getSize())) {
                 JOptionPane.showMessageDialog(this, "Cập nhật thông tin sản phẩm thành công!");
                 listProduct = productBUS.loadDataProduct();
